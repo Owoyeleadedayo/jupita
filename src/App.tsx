744fdf56d085fac9ff60c494e45dfc8897427eab
Owoyeleadedayo <owoyeleadedayo25@gmail.com>
2024-10-24 23:12:17 +0100

    feat:made update on the dashboard mobile resp

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter,  Route,  Routes } from 'react-router-dom'
 import DashboardPage from './pages/DashboardPage'
+import TransactionPage from './pages/TransactionPage';
+import Sidebar from './components/Sidebar';
 // import SignIn from './components/SignIn'
 
 function App() {
 
   return (
-   <>
+    <>
       <BrowserRouter>
-          {/* <SignIn /> */}
+        {/* <SignIn /> */}
+        <Sidebar />
         <Routes>
-          <Route path='/dashboard' element={<DashboardPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/transaction" element={<TransactionPage />} />
         </Routes>
       </BrowserRouter>
-   </>
-  )
+    </>
+  );
 }
 
 export default App
